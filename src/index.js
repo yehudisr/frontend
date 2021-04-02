f0371da8b8b8dc31966b3c6ce46875e6f8307cae
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-01 22:51:38 -0500

    styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const url = 'http://localhost:3000/users'
-const ulAnecdotes = document.querySelector('ul.anecdotes')     
+const ulAnecdotes = document.querySelector('ul.anecdotes')
 const ulMembers = document.querySelector('ul.members')
 const anecdoteForm = document.querySelector('form.anecdote-form')
 const newMemberForm = document.querySelector('form#new-family-member-form')
@@ -9,14 +9,14 @@ const addButton = document.querySelector('button#new-family-member')
 
 
 
-function renderHighLightedMember(familyMember){
- 
+function renderHighLightedMember(familyMember) {
+
     const h2 = document.querySelector('h2.card-title')
     h2.textContent = familyMember.name
 
     const memberImg = document.querySelector('img')
     memberImg.src = familyMember.image
-    
+
     const pCategory = document.querySelector('p.card-subtitle')
     pCategory.textContent = familyMember.category
 
@@ -32,23 +32,23 @@ function renderHighLightedMember(familyMember){
 
 
 fetch(`${url}/1`)
-  .then(response => response.json())
-  .then(user => {
-    renderHighLightedMember(user)
-    newMemberForm.dataset.id = user.id
-    user.family_members.forEach(familyMember => {
-        const li = document.createElement('li')
-        li.innerText = familyMember.name
-        li.dataset.id = familyMember.id
-        ulMembers.append(li)
-          
-    })
-  })
+    .then(response => response.json())
+    .then(user => {
+        renderHighLightedMember(user)
+        newMemberForm.dataset.id = user.id
+        user.family_members.forEach(familyMember => {
+            const li = document.createElement('li')
+            li.innerText = familyMember.name
+            li.dataset.id = familyMember.id
+            ulMembers.append(li)
 
-  ulMembers.addEventListener('click', event => {
+        })
+    })
+
+ulMembers.addEventListener('click', event => {
     if (event.target.matches('li')) {
         const id = event.target.dataset.id
-         fetch(`http://localhost:3000/family_members/${id}`)
+        fetch(`http://localhost:3000/family_members/${id}`)
             .then(response => response.json())
             .then(familyMember => {
                 renderHighLightedMember(familyMember)
@@ -57,11 +57,13 @@ fetch(`${url}/1`)
                     const anecdoteLi = document.createElement('li')
                     anecdoteLi.dataset.id = anecdote.id
                     anecdoteLi.innerText = anecdote.note
+                    anecdoteLi.classList.add('anecdote-li')
                     const deleteButton = document.createElement('button')
                     deleteButton.textContent = 'X'
+                    deleteButton.classList.add('anecdote-delete-button')
                     anecdoteLi.append(deleteButton)
                     ulAnecdotes.append(anecdoteLi)
-                   }) 
+                })
             })
     }
 })
@@ -70,43 +72,43 @@ fetch(`${url}/1`)
 
 anecdoteForm.addEventListener('submit', event => {
     event.preventDefault()
-    
+
     const id = event.target.dataset.id
     const newAnecdote = event.target[0].value
-   
+
 
     fetch(`http://localhost:3000/family_members/${id}/anecdotes`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({note: newAnecdote, family_member_id: id})
+        body: JSON.stringify({ note: newAnecdote, family_member_id: id })
     })
-    .then(response => response.json())
-    .then(anecdote => {
-        const li = document.createElement('li')
-        li.textContent = anecdote.note
-        li.dataset.id = anecdote.family_member_id
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = 'X'
-        li.append(deleteButton)
-        ulAnecdotes.append(li)
+        .then(response => response.json())
+        .then(anecdote => {
+            const li = document.createElement('li')
+            li.textContent = anecdote.note
+            li.dataset.id = anecdote.family_member_id
+            const deleteButton = document.createElement('button')
+            deleteButton.textContent = 'X'
+            li.append(deleteButton)
+            ulAnecdotes.append(li)
         })
 
-        event.target.reset()
+    event.target.reset()
 })
 
 
 newMemberForm.addEventListener('submit', event => {
     event.preventDefault()
-    
+
     const name = event.target.name.value
     const birthday = event.target.birthday.value
     const image = event.target.photo.value
     const category = event.target.category.value
     const user_id = event.target.dataset.id
 
-   
+
 
     newMember = { name, birthday, image, category, user_id }
 
@@ -117,15 +119,14 @@ newMemberForm.addEventListener('submit', event => {
         },
         body: JSON.stringify(newMember)
     })
-    .then(response => response.json())
-    .then(familyMember => {
-        renderHighLightedMember(familyMember)
+        .then(response => response.json())
+        .then(familyMember => {
+            renderHighLightedMember(familyMember)
         })
 })
 
-addButton.addEventListener('click', event =>{
-    console.log(event.target)
-    if(event.target.matches('button#new-family-member')){
+addButton.addEventListener('click', event => {
+    if (event.target.matches('button#new-family-member')) {
         newMemberForm.style.display = newMemberForm.style.display === 'block' ? 'none' : 'block'
     }
 })
@@ -145,25 +146,25 @@ updateMemberForm.addEventListener('submit', event => {
         },
         body: JSON.stringify(updatedValues)
     })
-    .then(response => response.json())
-    .then(familyMember => {
-        renderHighLightedMember(familyMember)
+        .then(response => response.json())
+        .then(familyMember => {
+            renderHighLightedMember(familyMember)
         })
 
 })
 
 ulAnecdotes.addEventListener('click', event => {
-    if (event.target.matches('button')){
+    if (event.target.matches('button')) {
         const id = event.target.closest('li').dataset.id
         fetch(`http://localhost:3000/anecdotes/${id}`, {
             method: 'DELETE'
         })
-        .then(response => response.json())
-        .then(data => console.log(data))
+            .then(response => response.json())
+            .then(data => console.log(data))
     }
 })
 
 
-updateBtn.addEventListener('click', event =>{
+updateBtn.addEventListener('click', event => {
     updateMemberForm.style.display = updateMemberForm.style.display === 'block' ? 'none' : 'block'
 })
